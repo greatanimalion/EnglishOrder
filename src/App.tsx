8296df36c3bc341dde94c6c19109e91d74e46c64
大动物 <15294745236@163.com>
2024-09-12 22:44:58 +0800

    :memo:init

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import Logo from '@/components/Logo'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, ConfigProvider, Layout, Menu, theme } from 'antd';
+import { Switch } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  //主题默认为true，亮
+  const [themValue, setThemValue] = useState<boolean>(true);
+  const setTheme = (checked: boolean) => {
+    setThemValue(checked)
+  }
+  const {
+    
+  } =theme.useToken();
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm:
+          themValue? theme.defaultAlgorithm : theme.darkAlgorithm
+      }}
+    >
+      <Layout style={{ height: '100vh'}}  >
+        <Sider trigger={null} collapsible collapsed={collapsed} style={{backgroundColor:'#141414'}}> 
+          <Logo title='四六级数据' showTitle={!collapsed}></Logo>
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme='light'
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: '近年数据',
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2',
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background:"#141414" }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+            <Switch checkedChildren="亮" unCheckedChildren="暗" defaultChecked onChange={setTheme} />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: '#141414',
+              borderRadius: '8px',
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+  );
+};
+
+export default App;
